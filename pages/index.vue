65e060a3af95fb712a9c7195c5a5cd3f6b1295db
Burak <148650898+BGNDGN@users.noreply.github.com>
2025-03-18 23:00:24 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <hr>
 
     <div v-if="users && users.length">
-      <ul class="user-list">a
+      <ul class="user-list">
         <li v-for="user in users" :key="user._id" class="user-item">
           <strong>Ad:</strong> {{ user.name }} <br />
           <strong>Email:</strong> {{ user.email }} <br />
